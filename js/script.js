67fd8fd81c518fd4d4588dd8b478db29a9fc4b0f
heejeong17 <92347216+heejeong17@users.noreply.github.com>
2021-12-06 09:35:40 +0900

    연습
    엽습

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  
+    
+
+    $(".slider-wrap02").slick({
+      asNavFor: ".slider-wrap01" ,
+      
+      
+      
+    });
+
+    $(".slider-wrap01").slick({
+      
+      
+    });
+
+    $(".slider-wrap03").slick({
+      asNavFor: ".slider-wrap01" ,
+      
+      
+      
+    });
+
+    $(".slider-wrap04").slick({
+      dots : true ,
+
+      
+
+      
+      
+    });
+
+    $(".slider-wrap05").slick({
+      
+
+      
+      
+    });
+    
+  });
